98a187f21640633f17bf36e10d4a4474a3a64a49
Alessio "Yagotzirck" Simbula <yagotzirck@gmail.com>
2023-06-16 00:14:29 +0200

    Replaced 'find' with logical indexing, for performance reasons

--- matlab/find_sift.m
+++ matlab/find_sift.m
@@ -134,7 +134,7 @@ sift_arr_norm = sift_arr(normalize_ind,:);
 sift_arr_norm = sift_arr_norm ./ repmat(tmp(normalize_ind,:), [1 size(sift_arr,2)]);
 
 % suppress large gradients
-sift_arr_norm(find(sift_arr_norm > 0.2)) = 0.2;
+sift_arr_norm(sift_arr_norm > 0.2) = 0.2;
 
 % finally, renormalize to unit length
 tmp = sqrt(sum(sift_arr_norm.^2, 2));
