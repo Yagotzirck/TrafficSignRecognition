b398558bec5606f8dc93b5385605a27c282aab5d
Alessio "Yagotzirck" Simbula <yagotzirck@gmail.com>
2023-06-21 13:06:35 +0200

    Implemented RANSAC with affine transformation matrix for matching points

--- matlab/find_best_match.m
+++ matlab/find_best_match.m
@@ -1,5 +1,5 @@
 function bestImageIndices = find_best_match(desc_train, desc_test, bof_chi2dist, k)
-%COUNT_INLIERS Returns a list of matching training images' indices for each test image.
+%FIND_BEST_MATCH Returns a list of matching training images' indices for each test image.
 
 %   Given two descriptor lists for training and test images, for each test
 %   image T we compare T with the k closest training images (where the distance
@@ -9,6 +9,7 @@ function bestImageIndices = find_best_match(desc_train, desc_test, bof_chi2dist,
 %   common with the test image T becomes the new "best match".
 
 
+showMatches = true;
 
 [~,mi] = mink(bof_chi2dist, k, 2);
 
@@ -32,6 +33,10 @@ for i = 1:numTestImgs
     % Esegui l'algoritmo RANSAC per trovare la migliore immagine
     bestInliersCount = 0;
     bestImageIndex = mi(i,1);   % By default, we take the closest image reported by BoW Chi-2
+
+    if showMatches
+        imgTest = imread(desc_test(i).imgfname);
+    end
     
     for j = 1:k
         % Seleziona l'immagine corrente dalla lista delle k immagini più simili
@@ -46,25 +51,59 @@ for i = 1:numTestImgs
 
         % Find the matching SIFT points between the current train and test
         % images
-        matchingPts = match_sift_pts( ...
-            currTestImgSIFTs, currTrainImgSIFTs, ...
-            currTestImgSIFTsCoords, currTrainImgSIFTsCoords, 0.6);
+%         matchingPts = match_sift_pts( ...
+%             currTestImgSIFTs, currTrainImgSIFTs, ...
+%             currTestImgSIFTsCoords, currTrainImgSIFTsCoords, 0.6);
 
-        if(size(matchingPts,1) >= 4)
 
-            trainMatchingPts =  matchingPts(:,1:2)';
-            testMatchingPts =   matchingPts(:,3:4)';
+        matchingPts1 = currTestImgSIFTsCoords;
+        matchingPts2 = currTrainImgSIFTsCoords;
 
-            % Normalize x and y relatively to width and height
-            
+        try
+            [tform, inliers1, inliers2] = ...
+            estimateGeometricTransform(matchingPts1, matchingPts2, 'affine');
+            currInliersCnt = size(inliers1,1);
         
-            [~, inliers] = ransacfithomography( ...
-                matchingPts(:,1:2)', matchingPts(:,3:4)', 0.005);
-    
-            currInliersCnt = size(inliers,1);
-        else
+            success = true;
+      
+        catch
             currInliersCnt = 0;
+            success = false;
         end
+
+        if showMatches && success
+            imgTrain = imread(desc_train(currTrainIdx).imgfname);
+
+            figure;
+            showMatchedFeatures(imgTest, imgTrain, ...
+                matchingPts1, matchingPts2, 'montage');
+            title('Putatively Matched Points (Including Outliers)');
+
+            figure;
+            showMatchedFeatures(imgTest, imgTrain, ...
+                inliers1, inliers2, 'montage');
+            title('Matched Points (Inliers Only)');
+
+            pause;
+
+            close all;
+        end
+
+%         if(size(matchingPts,1) >= 4)
+% 
+%             trainMatchingPts =  matchingPts(:,1:2)';
+%             testMatchingPts =   matchingPts(:,3:4)';
+% 
+%             % Normalize x and y relatively to width and height
+%             
+%         
+%             [~, inliers] = ransacfithomography( ...
+%                 matchingPts(:,1:2)', matchingPts(:,3:4)', 0.005);
+%     
+%             currInliersCnt = size(inliers,1);
+%         else
+%             currInliersCnt = 0;
+%         end
 
 
         
